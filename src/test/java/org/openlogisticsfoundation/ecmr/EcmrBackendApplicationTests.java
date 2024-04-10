1e303baa9c4f6c93453cdca1d8bccd8165e59b58
Artur Blek <Artur.Blek@de.rhenus.com>
2024-04-10 12:57:34 +0200

    Initial

---
+++ src/test/java/org/openlogisticsfoundation/ecmr/EcmrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openlogisticsfoundation.ecmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
