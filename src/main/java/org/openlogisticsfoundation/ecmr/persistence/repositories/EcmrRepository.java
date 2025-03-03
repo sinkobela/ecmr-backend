e5bd449a84a8dff1803dc2e37d3c0cd51c55de00
Lisa Gebel <lisa.gebel@iml.fraunhofer.de>
2025-03-03 09:04:44 +0000

    Fix ecmr query

--- src/main/java/org/openlogisticsfoundation/ecmr/persistence/repositories/EcmrRepository.java
+++ src/main/java/org/openlogisticsfoundation/ecmr/persistence/repositories/EcmrRepository.java
@@ -33,20 +33,20 @@ public interface EcmrRepository extends JpaRepository<EcmrEntity, Long> {
 
     @EntityGraph(value = "Ecmr.all", type = EntityGraph.EntityGraphType.FETCH)
     @Query("SELECT Distinct e FROM EcmrEntity e "
-            + "WHERE e.type = :type "
-            + "AND (:referenceId is null or e.referenceIdentificationNumber LIKE %:referenceId%) "
-            + "AND (:from is null or e.senderInformation.nameCompany LIKE %:from%)"
-            + "AND (:to is null or e.consigneeInformation.nameCompany LIKE %:to%)"
-            + "AND (:ecmrStatus is null or e.ecmrStatus = :ecmrStatus)"
-            + "AND (:licensePlate is null or e.carrierInformation.carrierLicensePlate LIKE %:licensePlate%)"
-            + "AND (:carrierName is null or e.carrierInformation.nameCompany LIKE %:carrierName%)"
-            + "AND (:carrierPostCode is null or e.carrierInformation.postcode LIKE %:carrierPostCode%)"
-            + "AND (:consigneePostCode is null or e.consigneeInformation.postcode LIKE %:consigneePostCode%)"
-            + "AND (:lastEditor is null or e.editedBy LIKE %:lastEditor% )"
-            + "AND (:ecmrTransportType is null OR "
-            + "((:ecmrTransportType = 'National' AND e.senderInformation.countryCode = e.consigneeInformation.countryCode)"
-            + "OR (:ecmrTransportType = 'International' AND e.senderInformation.countryCode != e.consigneeInformation.countryCode)))"
-            + "AND e.id IN (SELECT ea.ecmr.id FROM EcmrAssignmentEntity ea WHERE ea.group.id in :groupIds)")
+        + "WHERE e.type = :type "
+        + "AND (:referenceId is null or e.referenceIdentificationNumber LIKE CONCAT('%', :referenceId, '%')) "
+        + "AND (:from is null or e.senderInformation.nameCompany LIKE  CONCAT('%', :from, '%')) "
+        + "AND (:to is null or e.consigneeInformation.nameCompany LIKE  CONCAT('%', :to, '%')) "
+        + "AND (:ecmrStatus is null or e.ecmrStatus = :ecmrStatus)"
+        + "AND (:licensePlate is null or e.carrierInformation.carrierLicensePlate LIKE  CONCAT('%', :licensePlate, '%')) "
+        + "AND (:carrierName is null or e.carrierInformation.nameCompany LIKE  CONCAT('%', :carrierName, '%')) "
+        + "AND (:carrierPostCode is null or e.carrierInformation.postcode LIKE  CONCAT('%', :carrierPostCode, '%')) "
+        + "AND (:consigneePostCode is null or e.consigneeInformation.postcode LIKE  CONCAT('%', :consigneePostCode, '%')) "
+        + "AND (:lastEditor is null or e.editedBy LIKE CONCAT('%', :lastEditor, '%')) "
+        + "AND (:ecmrTransportType is null OR "
+        + "((:ecmrTransportType = 'National' AND e.senderInformation.countryCode = e.consigneeInformation.countryCode)"
+        + "OR (:ecmrTransportType = 'International' AND e.senderInformation.countryCode != e.consigneeInformation.countryCode)))"
+        + "AND e.id IN (SELECT ea.ecmr.id FROM EcmrAssignmentEntity ea WHERE ea.group.id in :groupIds)")
     Page<EcmrEntity> findAllByTypeAndAssignedGroupIds(@Param("type") EcmrType type, @Param("groupIds") List<Long> groupIds,
             @Param("referenceId") String referenceId, @Param("from") String from, @Param("to") String to,
             @Param("ecmrTransportType") String ecmrTransportType, @Param("ecmrStatus") EcmrStatus ecmrStatus,
